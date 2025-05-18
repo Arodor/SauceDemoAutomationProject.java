eb64f31f410e56a6a273c141f013a9b96d66b794
Arodor <hristobalinov@gmai.com>
2025-05-18 10:23:38 +0300

    - Geter for dropdown menu in order to use assert

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
-Match the input data from setup.java and recource property file
+## Project Overview
+Automated UI tests for SauceDemo using Selenium WebDriver, TestNG, and the Page Object Model.
+
+## Prerequisites
+- Java 17+
+- Maven 3.8+
+- Chrome/Firefox/Safari browser
+- Internet connection (for WebDriverManager)
+
+## Setup Instructions
+1. Clone the repository.
+2. Update `src/test/resources/resources.properties` with your desired configuration.
+3. Run `mvn clean test` to execute all tests.
+
+## Project Structure
+- `src/main/java/Pages/` - Page Object classes
+- `src/test/java/LoginTests/` - Test classes
+- `src/test/resources/resources.properties` - Test configuration
+
+## How to Run Tests
+```sh
+mvn clean testMatch the input data from setup.java and recource property file
 locations : src/test/resources/resources.properties  | https://github.com/Arodor/SauceDemoAutomationProject.java/blob/master/src/test/java/Utility/setup.java
 This is yet not complete project and it will evolve in the future
--- src/main/java/Pages/ProductPage.java
+++ src/main/java/Pages/ProductPage.java
@@ -34,12 +34,26 @@ public class ProductPage {
   return Integer.parseInt(CartItemNumber.getText());
 
  }
-    public  void sortByPriceLowToHigh() {
-        Select select = new Select(sortDropdown);
-        select.selectByVisibleText("Price (low to high)");
+ public void sortByPriceLowToHigh() {
 
+     Select select = new Select(sortDropdown);
+     select.selectByVisibleText("Price (low to high)");
 
-    }
+     // Get the price of the first item
+     WebElement firstItem = inventory.get(0);
+     String firstPriceText = firstItem.findElement(By.className("inventory_item_price")).getText().replace("$", "");
+     double firstItemPrice = Double.parseDouble(firstPriceText);
+
+     // Validate the first item is the cheapest
+     for (WebElement item : inventory) {
+         String priceText = item.findElement(By.className("inventory_item_price")).getText().replace("$", "");
+         double price = Double.parseDouble(priceText);
+         if (firstItemPrice > price) {
+             throw new AssertionError("First item is not the cheapest after sorting!");
+         }
+     }
+ }
+
     public void buyItem(){
         WebElement firstItem = inventory.getFirst();
         WebElement addToCartButton = firstItem.findElement(By.xpath(".//button[contains(text(), 'Add to cart')]"));
